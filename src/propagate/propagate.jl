2d96acc4d3a351c451b6fbcb484f676a277129a4
Peizhi Niu <npz_liekon@sjtu.edu.cn>
2023-07-03 10:25:26 -0400

    split

--- src/propagate/propagate.jl
+++ src/propagate/propagate.jl
@@ -35,72 +35,120 @@ function propagate(prop_method::ForwardProp, onnx_model_path, Flux_model, input_
     comp_graph = ONNXNaiveNASflux.load(onnx_model_path, infer_shapes=false)
     batch_info = Dict()
     global_info = Dict()
+    push!(global_info, "activation_number" => 0)
+    push!(global_info, "activation_node" => [])
+    push!(global_info, "final_node" => [])
     for (index, vertex) in enumerate(ONNXNaiveNASflux.vertices(comp_graph))
         if index == 1 # the vertex which index == 1 has no useful information, so it's output node will be the start node of the model
-            push!(global_info, "start_node" => []) #creat a array for storing the start_nodes because maybe there are more than 1 start_node
-            for node in outputs(vertex)
-                push!(global_info["start_node"], NaiveNASflux.name(node))
-            end
+            push!(global_info, "start_node" => [NaiveNASflux.name(output_node) for output_node in outputs(vertex)]) 
             continue
         end 
-    
+        
+        node_name = NaiveNASflux.name(vertex)
         new_dict = Dict() # store the information of this vertex 
         push!(new_dict, "vertex" => vertex)
         push!(new_dict, "layer" => NaiveNASflux.layer(vertex))
         push!(new_dict, "index" => index)
-        push!(new_dict, "inputs" => inputs(vertex))# note: inputs(vertex)) is not a string, use NaiveNASflux.name convert them to string 
-        push!(new_dict, "outputs" => outputs(vertex))# note: outputs(vertex)) is not a string, use NaiveNASflux.name convert them to string 
+        push!(new_dict, "outputs" => [NaiveNASflux.name(output_node) for output_node in outputs(vertex)])
+        # add input nodes of current node. If the input nodes of current node have activation(except identity), then the "inputs" should be the activation node
+        if !(node_name in global_info["start_node"])# if current node is not one of the start node
+            push!(new_dict, "inputs" => [])
+            for input_node in inputs(vertex)
+                input_node_name = NaiveNASflux.name(input_node)
+                if hasfield(typeof(batch_info[input_node_name]["layer"]), :σ) && string(batch_info[input_node_name]["layer"].σ) != "identity"
+                    push!(new_dict["inputs"], batch_info[input_node_name]["outputs"][1])
+                else
+                    push!(new_dict["inputs"], input_node_name)
+                end
+            end
+        else
+            push!(new_dict, "inputs" => nothing)
+        end
         
-        #this part defines the layer of Flatten, relu, add and so on because the type of this layer's "layer(vertex)" is string(like "#213")
         if length(string(NaiveNASflux.name(vertex))) >= 7 && string(NaiveNASflux.name(vertex))[1:7] == "Flatten" 
             push!(new_dict, "layer" => Flux.flatten)
-        elseif length(string(NaiveNASflux.name(vertex))) >= 4 && string(NaiveNASflux.name(vertex))[1:4] == "relu" 
-            push!(new_dict, "layer" => NNlib.relu)
+            push!(batch_info, node_name => new_dict) #new_dict belongs to batch_info
         elseif length(string(NaiveNASflux.name(vertex))) >= 3 && string(NaiveNASflux.name(vertex))[1:3] == "add" 
             push!(new_dict, "layer" => +)
-        end
+            push!(batch_info, node_name => new_dict) #new_dict belongs to batch_info
+        elseif length(string(NaiveNASflux.name(vertex))) >= 4 && string(NaiveNASflux.name(vertex))[1:4] == "relu" 
+            global_info["activation_number"] += 1
+            node_name = "relu" * "_" * string(global_info["activation_number"]) #activate == "relu_5" doesn't mean this node is 5th relu node, but means this node is 5th activation node
+            push!(new_dict, "layer" => NNlib.relu)
+            push!(batch_info, node_name => new_dict) #new_dict belongs to batch_info
+            push!(global_info["activation_node"], node_name)
+        elseif hasfield(typeof(NaiveNASflux.layer(vertex)), :σ) && string(NaiveNASflux.layer(vertex).σ) != "identity"#split this layer into a linear layer and a activative layer
+            global_info["activation_number"] += 1
+            activation_name = string(NaiveNASflux.layer(vertex).σ) * "_" * string(global_info["activation_number"])
+            push!(new_dict, "outputs" => [activation_name]) #new_dict store the information of the linear layer
+            push!(batch_info, node_name => new_dict) #new_dict belongs to batch_info
+                
+            activation_new_dict = Dict()#store the information of the activative layer
+            push!(activation_new_dict, "vertex" => vertex)
+            push!(activation_new_dict, "layer" => NaiveNASflux.layer(vertex).σ)
+            push!(activation_new_dict, "index" => index)# Do not need to change index
+            push!(activation_new_dict, "inputs" => [node_name])
+            push!(activation_new_dict, "outputs" => [NaiveNASflux.name(output_nodes) for output_nodes in outputs(vertex)])
+            push!(batch_info, activation_name => activation_new_dict)
+            push!(global_info["activation_node"], activation_name)
 
-        push!(batch_info, NaiveNASflux.name(vertex) => new_dict) #new_dict belongs to batch_info
-        if length(outputs(vertex)) == 0  #the final node has no output nodes
-            global_info["final_node"] = NaiveNASflux.name(vertex)
+            node_name = activation_name #for getting the final_node
+        else
+            push!(batch_info, node_name => new_dict) #new_dict belongs to batch_info
+        end
+        
+        if length(batch_info[node_name]["outputs"]) == 0  #the final node has no output nodes
+            push!(global_info["final_node"], node_name) 
         end
     end
 
     #BFS
     queue = Queue{Any}()
     start_node = global_info["start_node"]
-    Isvisit = Dict()#determine whether the node is visited
+    Isqueue = Dict()#determine whether the node is visited
     for node in start_node
         enqueue!(queue, node)
-        batch_info[node]["inputs"] = nothing #start_nodes have no input nodes
     end
 
     while !isempty(queue)
         node = dequeue!(queue)
-        if haskey(Isvisit, node) #means that this node has been visited
+        push!(Isqueue, node => false)
+ 
+        if haskey(batch_info[node], "output_bound")
             continue
         end
-        push!(Isvisit, node => true)
-        for node in batch_info[node]["outputs"]
-            if haskey(Isvisit, node) #means that this node has been visited
+
+        for output_node in batch_info[node]["outputs"]
+            if haskey(batch_info[output_node], "output_bound") || (haskey(Isqueue, output_node) && Isqueue[output_node] == true)
                 continue
             end
-            enqueue!(queue, NaiveNASflux.name(node))
+            enqueue!(queue, output_node)
+            push!(Isqueue, output_node => true)
         end
 
         if isnothing(batch_info[node]["inputs"])
             current_batch_bound = batch_bound
             batch_bound, aux_batch_info = forward_layer(prop_method, batch_info[node]["layer"], current_batch_bound, aux_batch_info)
         elseif length(batch_info[node]["inputs"]) == 2
-            input_node1 = NaiveNASflux.name(batch_info[node]["inputs"][1])
-            input_node2 = NaiveNASflux.name(batch_info[node]["inputs"][2])
+            input_node1 = batch_info[node]["inputs"][1]
+            input_node2 = batch_info[node]["inputs"][2]
+            if (!haskey(batch_info[input_node1], "output_bound") || !haskey(batch_info[input_node2], "output_bound")) && Isqueue[node] == false
+                enqueue!(queue, node)
+                push!(Isqueue, node => true)
+                continue
+            end
             current_batch_bound1 = batch_info[input_node1]["output_bound"]
             current_batch_bound2 = batch_info[input_node2]["output_bound"]
             aux_batch_info1 = batch_info[input_node1]["aux_batch_info"]
             aux_batch_info2 = batch_info[input_node2]["aux_batch_info"]
             batch_bound, aux_batch_info = forward_skip_batch(prop_method, batch_info[node]["layer"], current_batch_bound1, current_batch_bound2, aux_batch_info1, aux_batch_info2)
         else #length(batch_info[node][inputs] == 1
-            input_node = NaiveNASflux.name(batch_info[node]["inputs"][1])
+            input_node = batch_info[node]["inputs"][1]
+            if !haskey(batch_info[input_node], "output_bound") && Isqueue[node] == false
+                enqueue!(queue, node)
+                push!(Isqueue, node => true)
+                continue
+            end
             current_batch_bound = batch_info[input_node]["output_bound"]
             aux_batch_info = batch_info[input_node]["aux_batch_info"]
             batch_bound, aux_batch_info = forward_layer(prop_method, batch_info[node]["layer"], current_batch_bound, aux_batch_info)
@@ -109,9 +157,10 @@ function propagate(prop_method::ForwardProp, onnx_model_path, Flux_model, input_
         push!(batch_info[node], "aux_batch_info" => aux_batch_info)
     end     
 
-    final_node = global_info["final_node"]
+    final_node = global_info["final_node"][1]
     batch_bound = batch_info[final_node]["output_bound"]
     aux_batch_info = batch_info[final_node]["aux_batch_info"]
+    
 
     return batch_bound, aux_batch_info 
 end     
@@ -260,9 +309,9 @@ function forward_layer(prop_method, layer, batch_bound, batch_info)
         batch_bound, batch_info = forward_act_batch(prop_method, layer, batch_bound, batch_info)
     else
         batch_bound, batch_info = forward_linear_batch(prop_method, layer, batch_bound, batch_info)
-        if hasfield(typeof(layer), :σ)
-            batch_bound, batch_info = forward_act_batch(prop_method, layer.σ, batch_bound, batch_info)
-        end
+        #if hasfield(typeof(layer), :σ)
+        #    batch_bound, batch_info = forward_act_batch(prop_method, layer.σ, batch_bound, batch_info)
+        #end
     end
     return batch_bound, batch_info
 end
